a5a89a745cdd02548a4acd349a075712a9786a90
Lachlan Findlater <psyentist@madpsyence.com>
2024-12-05 23:19:11 +1300

    day1 and 2 in Go

---
+++ day1/day1.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+)
+
+func main() {
+	day1Part1()
+	day1Part2()
+}
+
+func day1Part1() {
+	list1, list2 := parseInput("day1")
+
+	slices.SortFunc(list1, func(i, j int) int {
+		return i - j
+	})
+
+	slices.SortFunc(list2, func(i, j int) int {
+		return i - j
+	})
+
+	answer := 0
+	for i := 0; i < len(list1); i++ {
+		answer = answer + int(math.Abs(float64(list1[i])-float64(list2[i])))
+	}
+
+	fmt.Printf("day 1 part 1: %d\n", answer)
+}
+
+func day1Part2() {
+	list1, list2 := parseInput("day1")
+	map1 := make(map[int]int)
+	map2 := make(map[int]int)
+
+	for index, ele := range list1 {
+		map1[ele]++
+		map2[list2[index]]++
+	}
+
+	answer := 0
+	for k := range map1 {
+		answer = answer + ((map1[k] * map2[k]) * k)
+	}
+
+	fmt.Printf("day 1 part 2: %d\n", answer)
+}
+
+func parseInput(fileName string) (numberList1, numberList2 []int) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+		return make([]int, 0), make([]int, 0)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	numberList1 = make([]int, 1)
+	numberList2 = make([]int, 1)
+
+	i := 1
+	for scanner.Scan() {
+		value, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if (i % 2) == 1 {
+			numberList1 = append(numberList1, value)
+		} else {
+			numberList2 = append(numberList2, value)
+		}
+		i++
+	}
+
+	return numberList1, numberList2
+}
---
+++ day2/day2.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	day2Part1()
+	day2Part2()
+}
+
+func day2Part1() {
+	reportList := parseInput("day2")
+
+	validReports := 0
+	for reportNumber, report := range reportList {
+		valid := true
+		if report[0] > report[1] {
+			for i := 1; i < len(report); i++ {
+				score := report[i-1] - report[i]
+				if score <= 0 || score > 3 {
+					valid = false
+					break
+				}
+			}
+			if valid {
+				fmt.Printf("report %d is valid\n", reportNumber)
+				validReports++
+			}
+		} else {
+			for i := 1; i < len(report); i++ {
+				score := report[i] - report[i-1]
+				if score <= 0 || score > 3 {
+					valid = false
+					break
+				}
+			}
+			if valid {
+				fmt.Printf("report %d is valid\n", reportNumber)
+				validReports++
+			}
+		}
+	}
+
+	fmt.Printf("day 2 part 1: %d\n", validReports)
+}
+
+func day2Part2() {
+	reportList := parseInput("day2")
+
+	validReports := 0
+	for _, report := range reportList {
+		if validateReport(report) {
+			validReports++
+		} else {
+			for index := range report {
+				newReport := removeElementFromSlice(index, report)
+				if validateReport(newReport) {
+					validReports++
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Printf("day 2 part 2: %d\n", validReports)
+}
+
+func validateReport(report []int) bool {
+	if len(report) < 2 {
+		return false
+	}
+
+	valid := true
+	if report[0] > report[1] {
+		for i := 1; i < len(report); i++ {
+			score := report[i-1] - report[i]
+			if score <= 0 || score > 3 {
+				valid = false
+				break
+			}
+		}
+	} else {
+		for i := 1; i < len(report); i++ {
+			score := report[i] - report[i-1]
+			if score <= 0 || score > 3 {
+				valid = false
+				break
+			}
+		}
+	}
+	return valid
+}
+
+func removeElementFromSlice(indexToRemove int, slice []int) []int {
+	newSlice := make([]int, len(slice)-1)
+	i := 0
+	for j, ele := range slice {
+		if j != indexToRemove {
+			newSlice[i] = ele
+			i++
+		}
+	}
+	return newSlice
+}
+
+func parseInput(fileName string) (reportList [][]int) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	reportList = make([][]int, 0)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		report := make([]int, 0)
+		for _, val := range line {
+			value, err := strconv.Atoi(val)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			report = append(report, value)
+		}
+
+		reportList = append(reportList, report)
+	}
+
+	return reportList
+}
